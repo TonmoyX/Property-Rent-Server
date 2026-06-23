f319f6d970731d1966d1202c36c8a8a44aa25f80
TonmoyX <142309932+TonmoyX@users.noreply.github.com>
2026-06-24 04:28:04 +0600

    jwt empliment

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 require('dotenv').config()
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 const port = process.env.PORT
 const uri = process.env.MONGODB_URI
 
@@ -17,7 +18,31 @@ const client = new MongoClient(uri, {
   }
 });
 
+const JWKS = createRemoteJWKSet(
+  new URL("http://localhost:3000/api/auth/jwks")
+)
 
+const verifyToken = async (req, res, next) =>{
+  const authHeader = req?.headers.authorization
+  if(!authHeader){
+    return res,status(401).json({message: "Unauthorized"});
+  }
+  const token = authHeader.split(' ')[1]
+  if(!token){
+    return res,status(401).json({message: "Unauthorized"});
+  }
+
+  try{
+    const {payload} = await jwtVerify(token, JWKS)
+    console.log(payload)
+    next()
+  }
+  catch(error){
+       return res,status(403).json({message: "Forbidden"});
+  }
+  // console.log(token)
+  // next()
+}
 
 async function run() {
   try {
@@ -43,7 +68,7 @@ async function run() {
       res.json(result)
     })
 
-    app.post('/addBookings', async (req, res) => {
+    app.post('/addBookings',verifyToken, async (req, res) => {
       const data = req.body;
       const result = await userBookingCollection.insertOne(data)
       res.json(result)
@@ -51,6 +76,13 @@ async function run() {
 
     app.get('/addBookings', async (req, res) => {
       const result = await userBookingCollection.find().toArray()
+      res.json(result)
+    })
+
+    app.patch('/addBookings/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await userBookingCollection.findOne(query)
       res.json(result)
     })
 
