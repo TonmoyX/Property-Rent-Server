6ed4e86ac7eb132b99893caffb5992c90b422b4c
TonmoyX <142309932+TonmoyX@users.noreply.github.com>
2026-06-24 14:36:40 +0600

    updated jwks url

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const client = new MongoClient(uri, {
 });
 
 const JWKS = createRemoteJWKSet(
-  new URL("http://localhost:3000/api/auth/jwks")
+  new URL("https://property-rent-client.vercel.app/api/auth/jwks")
 )
 
 const verifyToken = async (req, res, next) =>{
